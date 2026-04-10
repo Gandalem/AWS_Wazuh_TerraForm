fa9be905bcd3d6708a965985982d09e525831233
Gandalem <rbals8429@gmail.com>
2026-04-10 14:05:47 +0900

    wazuh데시보드 실행시 용량 부족으로 t3.medium -> t3.large 확장 용량 기본값에서 50기가로 확장

--- compute.tf
+++ compute.tf
@@ -17,10 +17,10 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "wazuh_server" {
   # 위에서 자동으로 찾은 최신 우분투 이미지의 ID를 여기에 쏙 넣습니다.
   ami           = data.aws_ami.ubuntu.id
-  # AWS Marketplace에서 'Wazuh'를 검색하여 제공되는 공식 무료 AMI ID를 입력해야 합니다.
-  # 아래는 임시 예시 값입니다. (리전마다 ID가 다름)
-  instance_type = "t3.medium"
+
   # Wazuh는 메모리 사용량이 많아 t3.medium 이상을 강력히 권장합니다.
+  instance_type = "t3.large" #dashboard 배포시 메모리 용량 부족으로 확장
+
   # 1단계의 퍼블릭 서브넷과 2단계의 보안 그룹을 연결
   subnet_id              = aws_subnet.wazuh_public_subnet.id
   vpc_security_group_ids = [aws_security_group.wazuh_sg.id]
@@ -30,6 +30,12 @@ resource "aws_instance" "wazuh_server" {
 
   # 외부에서 인터넷으로 접속하기 위해 공인 IP 할당을 활성화
   associate_public_ip_address = true
+
+  # 기본값 8기가는 너무 적어서 하드디스크 용량을 50GB로 확장 (추가된 부분!)
+  root_block_device {
+    volume_size = 50
+    volume_type = "gp3"
+  }
 
   tags = {
     Name = "wazuh-manager-node"
